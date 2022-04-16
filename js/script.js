4ba4ef55890fe621ff5f3e9f30357173a6460bf3
Mateusz Pitulski <jaskiermp@gmail.com>
2022-04-16 08:43:42 +0200

    ćwiczenie 2 skonczone

--- js/script.js
+++ js/script.js
@@ -5,13 +5,39 @@ console.log(listaKsiazek);
 const booksListElement = listaKsiazek.item(0);
 
 function render() {
-const thisBook = this;
-for (let book of dataSource.books) {
+    for (let book of dataSource.books) {
         const generatedHTML = bookTemplate(book);
         const DOMElement = utils.createDOMFromHTML(generatedHTML);
         booksListElement.appendChild(DOMElement);
-console.log(book);
     }
 }
 
 render();
+
+////////////////////////////////
+
+const favoriteBooks = [];
+const initActions = function () {
+    const listaOkładek = document.getElementsByClassName('book__image');
+    const listaOkładekTablica = Array.from(listaOkładek);
+    console.log(listaOkładekTablica);
+
+    for (let okładka of listaOkładekTablica) {
+        okładka.addEventListener('dblclick', function (event) {
+            event.preventDefault();
+            //console.log(event.target);
+            okładka.classList.add('favorite'); 
+            const clickedElement = okładka;
+            console.log(clickedElement);
+
+            //let element = document.getElementById('myDivID');
+            const dataID = clickedElement.getAttribute('data-id');
+            console.log(dataID);
+            favoriteBooks.push(dataID);
+            console.log(favoriteBooks);
+        });
+    }
+}
+
+
+initActions();
